239e40bcdfcbe8250b82597925fea4baa0f3ddb9
Joshua Lester <jlester3@ewu.edu>
2025-03-31 15:55:15 -0700

    Front page done

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -1,6 +1,26 @@
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/content']
-})
+  css: ['@fortawesome/fontawesome-svg-core/styles.css'],
+  build: {
+    transpile: ['vuetify'],
+  },
+  modules: [
+    '@nuxt/content',
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        // @ts-expect-error
+        config.plugins.push(vuetify({ autoImport: true }));
+      });
+    },
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+});

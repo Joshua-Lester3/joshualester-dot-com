3e4bc751a395df085a58865aa2322b9f84d73af4
Joshua Lester <jlester3@ewu.edu>
2025-04-03 12:23:50 -0700

    Added thank you message after submission of contact form

--- web/pages/contact.vue
+++ web/pages/contact.vue
@@ -1,22 +1,32 @@
 <template>
   <v-container>
-    <v-card color="tertiary" class="ma-3">
+    <v-card v-if="!submitted" color="tertiary" class="ma-3">
       <v-sheet color="secondary">
         <v-card-title>
           Contact Form
         </v-card-title>
       </v-sheet>
       <v-container>
-        <v-text-field max-width="400" label="Name" bg-color="primary" variant="solo" />
-        <v-text-field max-width="400" label="Email" bg-color="primary" variant="solo" />
-        <v-textarea label="Message" bg-color="primary" variant="solo" clearable />
+        <v-text-field max-width="400" label="Name" bg-color="primary" variant="solo" v-model="name" />
+        <v-text-field max-width="400" label="Email" bg-color="primary" variant="solo" v-model="email" />
+        <v-textarea label="Message" bg-color="primary" variant="solo" clearable v-model="message" />
       </v-container>
       <v-card-actions>
         <v-spacer />
-        <v-btn variant="elevated" color="secondary" class="mr-3 mb-3" @click="callLambdaFunction">
+        <v-btn variant="elevated" color="secondary" class="mr-3 mb-3" @click.once="callLambdaFunction">
           Submit
         </v-btn>
       </v-card-actions>
+    </v-card>
+    <v-card v-else color="tertiary" class="ma-3">
+      <v-sheet color="secondary">
+        <v-card-title>
+          Contact Form
+        </v-card-title>
+      </v-sheet>
+      <div class="ma-5 text-subtitle-1">
+        Thanks for reaching out! I'll try to get back to you as soon as possible :)
+      </div>
     </v-card>
   </v-container>
 </template>
@@ -26,6 +36,7 @@ import axios from 'axios';
 const name: Ref<string> = ref("");
 const email: Ref<string> = ref("");
 const message: Ref<string> = ref("");
+const submitted: Ref<boolean> = ref(false);
 
 async function callLambdaFunction() {
   try {
@@ -37,6 +48,7 @@ async function callLambdaFunction() {
         message: message.value
       }
     });
+    submitted.value = true;
   } catch (error) {
     console.error('Error calling lambda contact form function:', error)
   }

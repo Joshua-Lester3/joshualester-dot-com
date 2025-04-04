30a590ab9c0973e3aaea853aecb68343d65f6de2
Joshua Lester <jlester3@ewu.edu>
2025-04-04 13:07:45 -0700

    Changing config to allow for static hosting

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -6,8 +6,12 @@ export default defineNuxtConfig({
       link: [{ rel: 'icon', type: 'image/png', href: '/jl.png' }],
     },
   },
+  nitro: {
+    preset: 'static',
+  },
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  ssr: false,
   css: ['@fortawesome/fontawesome-svg-core/styles.css'],
   build: {
     transpile: ['vuetify'],

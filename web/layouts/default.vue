3de625db137468be721dff4bae523e04fe9a2dd7
Joshua Lester <127695056+Joshua-Lester3@users.noreply.github.com>
2025-04-07 13:58:59 -0700

    Update default.vue

--- web/layouts/default.vue
+++ web/layouts/default.vue
@@ -5,9 +5,9 @@
       <v-app-bar-title @click="router.push('/')" class="cursor-pointer">
         Joshua Lester
       </v-app-bar-title>
-      <template v-slot:append class="mr-5">
+      <!-- <template v-slot:append class="mr-5">
         <v-btn @click="router.push('/blogList')">Blog</v-btn>
-      </template>
+      </template> -->
     </v-app-bar>
     <v-main>
       <slot />

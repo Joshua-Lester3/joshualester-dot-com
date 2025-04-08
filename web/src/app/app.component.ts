4bba4034a11c3b1d16741cb7b247e8e413fe710f
Joshua Lester <jlester3@ewu.edu>
2025-04-08 08:32:34 -0700

    Toolbar done

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [MatSlideToggleModule],
+  imports: [
+    MatSlideToggleModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'web';
+  title = 'Joshua Lester';
+
+  goToLink(url: string) {
+    window.open(url, '_blank');
+  }
+
+  scrollTo(elementId: string) {
+    document.getElementById(elementId)?.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  constructor() {}
 }
